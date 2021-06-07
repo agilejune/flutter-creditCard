322dc4dc5e33c3dabed3df8a36e4ccd79906004a
agilejune <agile.dev1@mail.ru>
2021-06-07 19:27:57 +0900

    const CreditCardForm widget

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ## 0.1.2
-*   Lint warning fixes
+*   Added CreditCard with Form Widget `CreditCardForm`.
+*   Bug and Lint check fixes
 
 ## 0.1.1
 *   Code Improvements
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MySample());
 
-class MyApp extends StatelessWidget {
+class MySample extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MySampleState();
+  }
+}
+
+class MySampleState extends State<MySample> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,169 +27,40 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CreditCardExample(),
-    );
-  }
-}
-
-class CreditCardExample extends StatefulWidget {
-  const CreditCardExample({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _CreditCardExampleState createState() => _CreditCardExampleState();
-}
-
-class _CreditCardExampleState extends State<CreditCardExample> {
-  String cardNumber = '';
-  String expiryDate = '';
-  String cardHolderName = '';
-  String cvvCode = '';
-
-  final MaskedTextController _cardNumberController =
-      MaskedTextController(mask: '0000 0000 0000 0000');
-  final TextEditingController _expiryDateController =
-      MaskedTextController(mask: '00/00');
-  final TextEditingController _cardHolderNameController =
-      TextEditingController();
-  final TextEditingController _cvvCodeController =
-      MaskedTextController(mask: '0000');
-
-  FocusNode cvvFocusNode = FocusNode();
-
-  bool isCvvFocused = false;
-
-  void textFieldFocusDidChange() {
-    setState(() {
-      isCvvFocused = cvvFocusNode.hasFocus;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    cvvFocusNode.addListener(textFieldFocusDidChange);
-
-    _cardNumberController.addListener(() {
-      setState(() {
-        cardNumber = _cardNumberController.text;
-      });
-    });
-
-    _expiryDateController.addListener(() {
-      setState(() {
-        expiryDate = _expiryDateController.text;
-      });
-    });
-
-    _cardHolderNameController.addListener(() {
-      setState(() {
-        cardHolderName = _cardHolderNameController.text;
-      });
-    });
-
-    _cvvCodeController.addListener(() {
-      setState(() {
-        cvvCode = _cvvCodeController.text;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CreditCardWidget(
-              cardNumber: cardNumber,
-              expiryDate: expiryDate,
-              cardHolderName: cardHolderName,
-              cvvCode: cvvCode,
-              showBackView: isCvvFocused,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Form(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        margin:
-                            const EdgeInsets.only(left: 16, top: 16, right: 16),
-                        child: TextFormField(
-                          controller: _cardNumberController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Card number',
-                            hintText: 'xxxx xxxx xxxx xxxx',
-                          ),
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        margin:
-                            const EdgeInsets.only(left: 16, top: 8, right: 16),
-                        child: TextFormField(
-                          controller: _expiryDateController,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Expired Date',
-                              hintText: 'MM/YY'),
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        margin:
-                            const EdgeInsets.only(left: 16, top: 8, right: 16),
-                        child: TextFormField(
-                          controller: _cardHolderNameController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Card Holder',
-                          ),
-                          keyboardType: TextInputType.text,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        margin:
-                            const EdgeInsets.only(left: 16, top: 8, right: 16),
-                        child: TextField(
-                          focusNode: cvvFocusNode,
-                          controller: _cvvCodeController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'CVV',
-                            hintText: 'XXXX',
-                          ),
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.done,
-                          onChanged: (String cvvText) {
-                            setState(() {
-                              cvvCode = cvvText;
-                            });
-                          },
-                        ),
-                      ),
-                    ],
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CreditCardWidget(
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                showBackView: isCvvFocused,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: CreditCardForm(
+                    themeColor: Colors.red,
+                    onCreditCardModelChange: onCreditCardModelChange,
                   ),
                 ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void onCreditCardModelChange(CreditCardModel creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
